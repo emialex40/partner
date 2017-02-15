de4b834fd49c9a5bc916e9538c775b0e9762ea37
emialex40 <26emic73@gmail.com>
2017-02-15 20:11:32 +0200

    commit 15-02-2017

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+$('.carousel').carousel()
 
+function main() {
+		$('.menu-btn').click(function() {
+			$('.menu').animate({
+				'left': '0px'
+			}, 600);
+
+			// *********close-menu**********
+			$('.icon-close').click(function() {
+				$('.menu').animate({
+					'left': -320
+				}, 600);
+			});
+
+		});
+	};
+	main();
+
+$(".loader_inner").fadeOut();
+$(".loader").delay(200).fadeOut("slow");
+
+$(".logo img").addClass('animated flip');
 
 });
