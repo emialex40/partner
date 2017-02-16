62f89815461675ae57660133edfc78af24f00566
emialex40 <26emic73@gmail.com>
2017-02-16 20:43:47 +0200

    commit 16-02-2017

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,56 @@
 $(document).ready(function() {
 
-$('.sl').slick();
+$('.img_block').hover(
+	 function() {
+	 	$('.img_hover').css("display", "block");
+	},
 
+	 function() {
+ 		$('.img_hover').css("display", "none");
+	 });
 
+// slider slick
 
-$('.carousel').carousel({ interval: false });
+  $('.sl').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  arrows: true,
+  nextArrow: '<span class="prev"></span>',
+  prevArrow: '<span class="next"></span>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+// slider slick end
+
 
 function main() {
 		$('.menu-btn').click(function() {
