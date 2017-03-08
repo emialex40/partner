ae96eb251be24e669f4dbd504095403013c2db2e
emialex40 <26emic73@gmail.com>
2017-03-08 22:22:32 +0200

    wp_commit templates08032017

---
+++ wp-content/themes/partner_theme/templates/index.php
@@ -0,0 +1,143 @@
+<div id="content">
+    <div class="section why" id="anchor1">
+        <div class="container">
+            <div class="row">
+                <h2>6 причин працювати з нами</h2>
+                <div class="col-md-4 col-xs-12">
+                    <div class="icon-box">
+                        <img class="icon" src="<?php bloginfo('template_url'); ?>/img/icons/procent_color.png" alt="exclusive">
+                        <p>Ексклюзив</p>
+                    </div>
+                    <div class="icon-box-bottom">
+                        <img class="icon" src="<?php bloginfo('template_url'); ?>/img/icons/Track-Order-100.png" alt="Track-Order-100">
+                        <p>Іноваційні<br>технології</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-12">
+                    <div class="icon-box">
+                        <img class="icon" src="<?php bloginfo('template_url'); ?>/img/icons/Prize-100.png" alt="exclusive">
+                        <p class="expce">Досвід</p>
+                    </div>
+                    <div class="icon-box-bottom midle">
+                        <img class="icon" src="<?php bloginfo('template_url'); ?>/img/icons/marketing-icon.png" alt="Track-Order-100">
+                        <p class="marng">Індивідуальний<br>маркетинг</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-12">
+                    <div class="icon-box">
+                        <img class="icon" src="<?php bloginfo('template_url'); ?>/img/icons/Commercial-Development-Management-100.png" alt="exclusive">
+                        <p class="prtnr">Мережа партнерів</p>
+                    </div>
+                    <div class="icon-box-bottom">
+                        <img class="icon" src="<?php bloginfo('template_url'); ?>/img/icons/Combo-Chart-100.png" alt="Track-Order-100">
+                        <p class="perf">Вдосконалення<br>майстерності</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<div id="anchor2">
+    <div class="section blue">
+        <div class="container">
+            <div class="row">
+                <h2><?php echo get_cat_name(2);?></h2>
+                <div class="col-md-12">
+                    <div class="r_estate">
+                        <div class="sl">
+                                    <?php
+                                        $recent = new WP_Query('cat=2&nopaging=1');
+                                        while($recent -> have_posts()) : $recent -> the_post(); ?>
+                                        <div class="img_block">
+                                        <?php the_post_thumbnail('full'); ?>
+                                        <div class="img_hover" id="home_1">
+                                        <p><?php echo (get_post_meta($post->ID, 'кол-во комнат', true)); ?>,Площа: <?php echo (get_post_meta($post->ID, 'Пощадь', true)); ?>.кв</p>
+                                            <strong class="price"><?php echo (get_post_meta($post->ID, 'продажа-аренда', true)); ?></strong>
+                                            <a href="<?php the_permalink(); ?>"><span>докладно</span></a>
+                                        </div>
+                                    </div>
+                                    <?php wp_reset_postdata(); ?>
+                                    <?php endwhile; ?>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section blue">
+        <div class="container">
+            <div class="row">
+                <h2><?php echo get_cat_name(3);?></h2>
+                <div class="col-md-12">
+                    <div class="r_estate">
+                            <div class="sl">
+                            <?php 
+                                $recent = new WP_Query('cat=3&nopaging=1');
+                                while($recent -> have_posts()) : $recent -> the_post(); ?>
+                                        <div class="img_block">
+                                            <?php the_post_thumbnail('full'); ?>
+                                        <div class="img_hover" id="home_1">
+                                            <p><?php echo (get_post_meta($post->ID, 'кол-во комнат', true)); ?>,Площа: <?php echo (get_post_meta($post->ID, 'Пощадь', true)); ?>.кв</p>
+                                            <strong class="price"><?php echo (get_post_meta($post->ID, 'продажа-аренда', true)); ?></strong>
+                                            <a href="<?php the_permalink();?>"><span>докладно</span></a>
+                                        </div>
+                                    </div>
+                                    <?php wp_reset_postdata(); ?>
+                                <?php endwhile; ?>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section blue">
+        <div class="container">
+            <div class="row">
+                <h2><?php echo get_cat_name(4);?></h2>
+                <div class="col-md-12">
+                    <div class="r_estate">
+                        <div class="sl">
+                              <?php 
+                                $recent = new WP_Query('cat=4&nopaging=1');
+                                while($recent -> have_posts()) : $recent -> the_post(); ?>
+                                        <div class="img_block">
+                                            <?php the_post_thumbnail('full'); ?>
+                                        <div class="img_hover" id="home_1">
+                                            <p><?php echo (get_post_meta($post->ID, 'кол-во комнат', true)); ?>,Площа: <?php echo (get_post_meta($post->ID, 'Пощадь', true)); ?>.кв</p>
+                                            <strong class="price"><?php echo (get_post_meta($post->ID, 'продажа-аренда', true)); ?></strong>
+                                            <a href="<?php the_permalink();?>"><span>докладно</span></a>
+                                        </div>
+                                    </div>
+                                    <?php wp_reset_postdata(); ?>
+                                <?php endwhile; ?>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="section rev_section" id="anchor3">
+        <div class="container">
+            <div class="row">
+                    <h2><?php echo get_cat_name(5);?></h2>
+                <div class="col-md-12">
+                    <div class="r_estate">
+                        <div class="reviews">
+                            <?php
+                                $recent = new WP_Query('cat=5&nopaging=1');
+                                while($recent -> have_posts()) : $recent -> the_post(); ?>
+                                        <div class="img_block">
+                                            <div class="ava">
+                                                <?php the_post_thumbnail('full'); ?>
+                                            </div>
+                                        <h3><?php echo get_the_title( $post ) ?></h3>
+                                            <p><?php the_content( ); ?></p>
+                                </div>
+                                    <?php wp_reset_postdata(); ?>
+                                <?php endwhile; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
---
+++ wp-content/themes/partner_theme/templates/page.php
@@ -0,0 +1,95 @@
+<?php get_header() ?>
+
+<div id="content_block">
+    <div class="container">
+        <div class="row">
+
+    <?php
+    $post_info = get_post();
+    $post_id = (!empty($post_info->ID)) ? $post_info->ID : 0;
+    $query = new WP_Query( array( 'p' => $post_id ) );
+
+    while ($query -> have_posts()): $query -> the_post();
+        ?>
+
+        <div class="col-md-6">
+            <div class="fotorama" data-nav="thumbs" data-width="525" data-thumbwidth="110" data-thumbheight="80" data-allowfullscreen="true" data-loop="true">
+                <?php the_content( ); ?>
+            </div>
+
+            <div class="panorama">
+                <?php echo (get_post_meta($post->ID, 'panorama', true)); ?>
+            </div>
+
+            <div class="video-cont">
+                <?php echo (get_post_meta($post->ID, 'video', true)); ?>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="date_text">
+                <img src="<?php bloginfo('template_url'); ?>/img/icons/circle.png" alt="circle">
+                <p class="selling"><?php echo (get_post_meta($post->ID, 'продажа-аренда', true)); ?></p>
+                <p class="slash">/</p>
+                <p class="object_name"><?php echo (get_post_meta($post->ID, 'тип объекта', true)); ?></p>
+                <br>
+                <p class="public">Дата публикации:</p><p class="date_p"><?php the_date(); ?></p>
+            </div>
+            <div class="address">
+                <p class="street"><?php echo (get_post_meta($post->ID, 'adress', true)); ?></p>
+                <p>(</p><p><?php echo (get_post_meta($post->ID, 'город', true)); ?></p><p>)</p>
+            </div>
+            <div class="price_text">
+                <strong><?php echo (get_post_meta($post->ID, 'цена', true)); ?></strong>
+                <p>без комісії</p>
+            </div>
+            <div class="table_rev">
+                <table>
+                    <tr>
+                        <td><?php echo (get_post_meta($post->ID, 'кол-во комнат', true)); ?> кімнати</td>
+                        <td><?php echo (get_post_meta($post->ID, 'Пощадь', true)); ?><sup>2</sup></td>
+                        <td><?php echo (get_post_meta($post->ID, 'Этаж', true)); ?></td>
+                    </tr>
+                </table>
+            </div>
+            <input class="callbeck" type="submit" value="Замовити зворотній зв'язок">
+            <div class="info">
+                <table>
+                    <caption>Інформація</caption>
+                    <tr>
+                        <td>Загальна площа</td>
+                        <td><?php echo (get_post_meta($post->ID, 'Пощадь', true)); ?>м<sup>2</sup></td>
+                    </tr>
+                    <tr>
+                        <td>Житлова площа</td>
+                        <td><?php echo (get_post_meta($post->ID, 'Жилая площадь', true)); ?>м<sup>2</sup></td>
+                    </tr>
+                    <tr>
+                        <td>Площа кухні</td>
+                        <td><?php echo (get_post_meta($post->ID, 'Площадь кухни', true)); ?>м<sup>2</sup></td>
+                    </tr>
+                    <tr>
+                        <td>Кімнат</td>
+                        <td><?php echo (get_post_meta($post->ID, 'кол-во комнат', true)); ?></td>
+                    </tr>
+                    <tr>
+                        <td>Поверх</td>
+                        <td><?php echo (get_post_meta($post->ID, 'Этаж', true)); ?></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="description">
+                <h2>Опис</h2>
+                <p>
+                    <?php echo (get_post_meta($post->ID, 'Описание', true)); ?>
+                </p>
+            </div>
+        </div>
+        <?php break; ?>
+    <?php endwhile; ?>
+
+        </div>
+    </div>
+</div>
+
+<?php get_footer() ?>
