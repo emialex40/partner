7dda4fb3ec0d0276ced7833913ce3dfa3fec6b21
emialex40 <26emic73@gmail.com>
2017-03-20 16:24:11 +0200

    404 page

---
+++ wp-content/themes/partner_theme/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+			<section">
+				<div class="container">
+						<h1 class="error">Oops! That page can&rsquo;t be found</h1>
+						<img class ="img_error" src="<?php bloginfo('template_url'); ?>/img/404.jpg" alt="404">
+				</div>
+			</section><!-- .error-404 -->
+	</div>
+<?php get_footer(); ?>
